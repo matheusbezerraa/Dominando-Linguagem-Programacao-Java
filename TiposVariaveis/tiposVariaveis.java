ed58d4d3230807520090887b028230134147664f
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2023-05-05 16:02:11 -0300

    commit terminei tipos de variaveis

--- TiposVariaveis/tiposVariaveis.java
+++ TiposVariaveis/tiposVariaveis.java
@@ -1,8 +1,22 @@
 package TiposVariaveis;
 
 public class tiposVariaveis{
-    public static void main(String[] args) {
-        System.out.println("oi");
+    public static void main(String[] args) throws Exception {
+
+        String meuNome = "Matheus";
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+       int numero = 5;
+
+       numero = 10;
+
+       System.out.println(numero);
+
+       final double VALOR_DE_PI = 3.14; // para não alterar a variavel(usar caixa alta como boas praticas)
     } 
     
 }
