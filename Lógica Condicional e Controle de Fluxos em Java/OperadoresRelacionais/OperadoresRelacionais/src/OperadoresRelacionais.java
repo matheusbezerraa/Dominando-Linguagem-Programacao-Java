2492da790ea70f0ae3e701af3c88c21bb59dc652
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2023-05-09 17:59:51 -0300

    commit ajustando repositorio

---
+++ Lógica Condicional e Controle de Fluxos em Java/OperadoresRelacionais/OperadoresRelacionais/src/OperadoresRelacionais.java
@@ -0,0 +1,39 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        String s3 = "Cicrano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1597L;
+        long l2 = 8997L;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("i1 == i2 "+ (i1 == i2));
+        System.out.println("i1 != i2 "+ (i1 != i2));
+        System.out.println("i1 > i2 "+ (i1>i2));
+        System.out.println("i1<=i2 "+ (i1 <= i2));
+
+        System.out.println("f1 == f2 "+ (f1 == f2));
+        System.out.println("f1 != f2 "+ (f1 != f2));
+        System.out.println("f1 > f2 "+ (f1>f2));
+        System.out.println("f1<f2 "+ (f1 < f2));
+
+        System.out.println("c1 == c2 "+ (c1 == c2));
+        System.out.println("c1 != c2 "+ (c1 != c2));
+        System.out.println("c1 > c2 "+ (c1>c2));
+        System.out.println("c1<=c2 "+ (c1 <= c2));
+
+        // e por ai vai...
+    }
+}
