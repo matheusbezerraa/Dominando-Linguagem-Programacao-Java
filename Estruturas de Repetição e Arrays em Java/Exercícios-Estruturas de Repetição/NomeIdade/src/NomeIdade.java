3556eac2320d6346a37931dcc33ae978fc2f2436
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2023-07-21 16:04:42 -0300

    commit

--- Estruturas de Repetição e Arrays em Java/Exercícios-Estruturas de Repetição/NomeIdade/src/NomeIdade.java
+++ Estruturas de Repetição e Arrays em Java/Exercícios-Estruturas de Repetição/NomeIdade/src/NomeIdade.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class NomeIdade {
     public static void main(String[] args) throws Exception {
+
         Scanner scanner = new Scanner(System.in);
 
         String nome;
